661ff5a8082235b5839af9749aa97eda17019572
FizzBank <labrador.j.bscs@gmail.com>
2022-07-22 09:37:31 +0800

    Fizz Bank

---
+++ app/src/main/java/com/bank/izbank/UserInfo/History.java
@@ -0,0 +1,50 @@
+package com.bank.izbank.UserInfo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class History {
+    private String userId;
+    private String Process;
+    private Date date;
+    private int cash;
+
+    public History(String userId, String process, Date date) {
+        this.userId = userId;
+        Process = process;
+        this.date = date;
+    }
+
+    public History(String id, String process, Date date, int cash) {
+    }
+
+    public Date getDateDate() {
+
+        return date;
+    }
+    public String getDateString() {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        return format.format(date);
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProcess() {
+        return Process;
+    }
+
+    public void setProcess(String process) {
+        Process = process;
+    }
+}
